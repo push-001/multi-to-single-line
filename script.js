8e4b1176e0332bf91db55de2be882d1eae28c9d3
Pushpendra upadhyay <hspush5080@gmail.com>
2021-12-13 10:17:30 +0530

    Adding all the files

---
+++ script.js
@@ -0,0 +1,38 @@
+document.getElementById("copy-text-area")
+    .addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("submit-button").click();
+    }
+});
+
+function copyFunction() {
+  document.getElementById("success-alert").style.display = "block";
+  var content = document.getElementById("copy-text-area").value;
+  console.log(content);
+  content = content.replace(/[\r\n]+/g, "\n");
+  content = content.replace(/\n/g, " ");
+
+  console.log(content);
+  var textArea = document.createElement("textarea");
+  textArea.value = content;
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand("copy");
+    var msg = successful ? "successful" : "unsuccessful";
+  } catch (err) {
+    console.log("Fallback: Oops, unable to copy", err);
+  }
+  document.body.removeChild(textArea);
+
+  document.getElementById("copy-text-area").value = "";
+
+  let hideSeek = setTimeout(function showElement(){
+    document.getElementById("success-alert").style.display = "none";
+}, 2000);
+
+  
+}
